d9ec6d025777262ceb2126c597764111d380f660
Eric James Deiter <eric@deiter.com>
2017-06-30 03:39:49 -0400

    Neofetch installer and config

---
+++ installers/neofetch-config.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo bash -c "echo 'neofetch' >> /etc/bash.bashrc"
---
+++ installers/neofetch-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:dawidd0811/neofetch -y
+sudo apt-get update
+sudo apt-get install neofetch -y
