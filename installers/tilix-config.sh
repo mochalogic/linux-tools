00024e07806815b1bf9db412bd06c05f07097b6b
Eric James Deiter <eric@deiter.com>
2017-06-30 03:39:35 -0400

    Tilix comment

--- installers/tilix-config.sh
+++ installers/tilix-config.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 #https://github.com/gnunn1/tilix/issues/571
 dconf load /com/gexperts/Tilix/ < ./configs/tilix.dconf
+
+#You might need to add this to ~/.bashrc or ~/etc/bash.bashrc
+#if [ $TILIX_ID ] || [ $VTE_VERSION ]; then
+#  source /etc/profile.d/vte.sh
