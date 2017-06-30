803bf35c645d6601ba4b6bd18db9b334419262f2
Eric James Deiter <eric@deiter.com>
2017-06-30 03:12:14 -0400

    Updates to tilix config

--- installers/tilix-config.sh
+++ installers/tilix-config.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#Load tilix with common config
 #https://github.com/gnunn1/tilix/issues/571
-dconf load /com/gexperts/Tilix/ < /configs/tilix.dconf
+dconf load /com/gexperts/Tilix/ < ./configs/tilix.dconf
