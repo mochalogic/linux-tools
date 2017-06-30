8138b0d76be75447146842600d1e6b27309c7508
Eric James Deiter <eric@deiter.com>
2017-06-30 03:06:07 -0400

    Added tilix configs

--- installers/install-tilix.sh
+++ installers/install-tilix.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo add-apt-repository ppa:webupd8team/terminix -y
 sudo apt-get update
 sudo apt-get install tilix -y
---
+++ installers/tilix-config-dump.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#https://github.com/gnunn1/tilix/issues/571
+dconf dump /com/gexperts/Tilix/ > ./configs/tilix.dconf
